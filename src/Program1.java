a1f677357503c354b2969b2831c1da6cd4512ff7
Bharath-ctrl <bharathkumar60404@gmail.com>
2022-03-14 12:42:46 +0530

    14/3/22

---
+++ src/Program1.java
@@ -0,0 +1,24 @@
+public class Program1 {
+
+        public static void main(String[] args) {
+            String name="bharat";
+            System.out.println("hello "+name);
+            Integer a=10,b=190,c;
+            c=b/a;
+            System.out.println("res: "+c);
+            c++;
+            System.out.println("res: "+c);
+            c--;
+            System.out.println("res:c-- :" +c);
+
+            if (b%2==0){
+                System.out.println(b+" is even");
+            }else{
+                System.out.println(b+" is odd");
+
+            }
+        }
+
+}
+
+
